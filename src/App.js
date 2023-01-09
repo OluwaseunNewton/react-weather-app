362db6769e71eb66ff2f229b6dac8bf332affe5c
OluwaseunNewton <113258885+OluwaseunNewton@users.noreply.github.com>
2023-01-09 11:25:35 +0100

    added the footer and link to GitHub

--- src/App.js
+++ src/App.js
@@ -2,5 +2,26 @@ import React from "react";
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by{" "}
+        <a
+          href="https://www.linkedin.com/in/oluwaseun-newton-m-d-mpa-3071b871/"
+          target="-blank"
+        >
+          Oluwaseun Newton
+        </a>{" "}
+        and it is
+        <a
+          href="https://github.com/OluwaseunNewton/react-weather-app"
+          target="-blank"
+        >
+          {" "}
+          Open Sourced on GitHub
+        </a>
+      </footer>
+    </div>
+  );
 }
