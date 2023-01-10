de831ee2753e73410450ef0753665166f6ef5cfc
OluwaseunNewton <113258885+OluwaseunNewton@users.noreply.github.com>
2023-01-10 11:03:47 +0100

    Added Bootstrap to the project

--- src/App.js
+++ src/App.js
@@ -4,24 +4,26 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project was coded by{" "}
-        <a
-          href="https://www.linkedin.com/in/oluwaseun-newton-m-d-mpa-3071b871/"
-          target="-blank"
-        >
-          Oluwaseun Newton
-        </a>{" "}
-        and it is
-        <a
-          href="https://github.com/OluwaseunNewton/react-weather-app"
-          target="-blank"
-        >
-          {" "}
-          Open Sourced on GitHub
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project was coded by{" "}
+          <a
+            href="https://www.linkedin.com/in/oluwaseun-newton-m-d-mpa-3071b871/"
+            target="-blank"
+          >
+            Oluwaseun Newton
+          </a>{" "}
+          and it is
+          <a
+            href="https://github.com/OluwaseunNewton/react-weather-app"
+            target="-blank"
+          >
+            {" "}
+            Open Sourced on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
